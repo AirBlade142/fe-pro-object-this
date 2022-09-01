5038a3e801e59c7b4117601f961bbb7ee6f34bd2
unknown <D:\The Bat!>
2022-09-01 15:02:54 +0300

    DONE

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-export const hotel = new Object();
+export const hotel = {
+  quantityOfPlaces: 30, //количевсто мест
+  priceByPlace: 20, // цена одного места в отеле
+  bankAccount: 0, // сколько денег на счету нашего отеля
+  guests: {},
+  getLength: function () {
+    return Object.keys(this.guests).length;
+  },
+  getActualFreePlace: function () {
+    return this.quantityOfPlaces - this.getLength();
+  },
+  paidPerPlace: function () {
+    this.bankAccount += this.priceByPlace;
+  },
+  checkInGuest: function (firstName, lastName, money) {
+    if (this.getActualFreePlace() < 1) {
+      return 'Sorry, we have not free spaces';
+    }
+    if (money < this.priceByPlace) {
+      return 'Sorry, you have not enough money';
+    }
+    this.guests[this.getLength()] = {
+      firstName: firstName,
+      lastName: lastName,
+      money: money - this.priceByPlace,
+    };
+    this.paidPerPlace();
+    return this.guests[this.getLength() - 1];
+  },
+};
